f3920a4075676a6c48bfa1e285715fdd79814b2f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-08-06 14:25:18 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	long long n;
+	cin >> n;
+	// remaining days
+	long long remaining = n % 7;
+	// total weeks
+	long long weeks = n / 7;
+	// minimum days off (initial)
+	long long mn = weeks * 2;
+	// maximum days off (initial)
+	long long mx = weeks * 2;
+	// to get the minimum, starting day must be MONDAY
+	if (remaining == 6) {
+		// if the remaining days is equal to 6, then add 1 day to the minimum
+		mn++;
+	}
+	// to get the maximum, starting day must be SATURDAY
+	if (remaining < 2) {
+		// if the remaining days is less than 2, then simply add it to the maximum
+		mx += remaining;
+	} else {
+		// otherwise, add 2 to the maximum
+		mx += 2;
+	}
+	cout << mn << " " << mx << '\n';
+	return 0;
+}
